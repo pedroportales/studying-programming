af1f9205d7af80ef1daa1e961abe1072e2dc0643
Pedro Portales <pedro.portales@protonmail.com>
2021-09-04 11:34:52 -0300

    desafio033.py

---
+++ python/CursoEmVideo/desafio033.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''
+Faça um programa que leia três números e mostre qual é o maior e qual é o menor
+'''
+
+n1 = int(input("Primeiro valor: "))
+n2 = int(input("Segundo valor: "))
+n3 = int(input("Terceiro valor: "))
+
+
+if n1 > n2 and n1 > n3:
+    maior = n1
+
+elif n2 > n1 and n2 > n3:
+    maior = n2
+
+elif n3 > n1 and n3 > n2:
+    maior = n3
+
+if n1 < n2 and n1 < n3:
+    menor = n1
+
+elif n2 < n1 and n2 < n3:
+    menor = n2
+
+elif n3 < n1 and n3 < n2:
+    menor = n3
+
+else:
+    print("Os valores são iguais!")
+
+print(f"O maior valor é: {maior}")
+print(f"O menor valor é: {menor}")
