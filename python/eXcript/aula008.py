098c9a77345b87f97727ed9b07051caed9fbafa3
Pedro Portales <pedro.portales@protonmail.com>
2021-10-31 20:00:00 -0300

    eXcript

---
+++ python/eXcript/aula008.py
@@ -0,0 +1,5 @@
+'''
+print("Aprendendo como utilizar comentários.")
+print("segunda linha")
+'''
+print("terceira linha") # aqui é um comentário que não será interpretado
---
+++ python/eXcript/aula013-014.py
@@ -0,0 +1,18 @@
+num_int = 5
+num_dec = 7.3
+val_str = "qualquer texto"
+
+# Inteiro
+print("o valor é:", num_int)
+print("o valor é: %i" %num_int)
+print("o valor é: " + str(num_int))
+
+# Float
+print("Concatenando decimal:", num_dec)
+print("Concatenando decimal: %.10f" %num_dec)
+print("Concatenando decimal: " + str(num_dec))
+
+# String
+print("Concatenando strings:", val_str)
+print("Concatenando strings: %s" %val_str)
+print("Concatenando strings: " + val_str)
