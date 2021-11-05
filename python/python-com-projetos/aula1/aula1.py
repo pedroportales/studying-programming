19b83a590ece4195ff2148a9ddc2aad144ab9110
Pedro Portales <pedro.portales@protonmail.com>
2021-11-05 19:16:18 -0300

    new python

---
+++ python/python-com-projetos/aula1/aula1.py
@@ -0,0 +1,13 @@
+'''
+# Comentário
+print('Linha 1')  # outro comentario
+print('Linha 2')
+# Uma linha
+# Outra linha
+print('Linha 3')  # Comentário
+# print('Qualquer coisa')
+print('Linha 4')  # mais um
+print('Linha 5')
+'''
+print('Linha 6')
+# Final
