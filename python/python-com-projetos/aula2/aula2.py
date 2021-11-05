7aeb7c0e0251842275b50903f71d640f77766bd0
Pedro Portales <pedro.portales@protonmail.com>
2021-11-05 19:49:47 -0300

    print

---
+++ python/python-com-projetos/aula2/aula2.py
@@ -0,0 +1,7 @@
+#print(123456)
+print('Luiz', 'Otávio', 'Outra coisa', sep="-", end="######")
+print("João", "e", "Maria", sep="-", end="")
+
+# CPF
+print("\n824", "176", "070", sep=".", end="-")
+print("18")
