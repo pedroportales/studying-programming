173c06e7263147a44f992a2a3c6a51e441a2ede8
Pedro Portales <pedro.portales@protonmail.com>
2021-11-07 18:46:13 -0300

    aula5

---
+++ python/python-com-projetos/aula5/aula5.py
@@ -0,0 +1,21 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+print('Multiplicação * ',10 * 10)
+print('Adição + ', 10 + 10)
+print('Subtração - ', 10 - 5)
+print('Divisão / ', 10 / 2)
+
+print('\n', 10 * 'Loop')
+print('5' + '5')
+print('Luiz' + ' ' + 'Otávio e ele tem ' + '32' + ' anos')
+
+print()
+print(10 / 3)
+print(10 // 3)
+print(2 ** 10)
+print(10 % 3)
+
+print()
+print(5 + 2 * 10)
+print((5 + 2) * 10)
