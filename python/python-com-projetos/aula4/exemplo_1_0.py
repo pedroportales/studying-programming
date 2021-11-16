bb02957e768e41719349dd84288cdbacc9c35290
Pedro Portales <pedro.portales@protonmail.com>
2021-11-16 10:46:31 -0300

    condicoes

---
+++ python/python-com-projetos/aula4/exemplo_1_0.py
@@ -0,0 +1,25 @@
+"""
+Condições IF, ELIF e ELSE - Aula 4
+"""
+
+if False:
+	print("Verdadeiro.")
+
+	n1 = 2
+	n2 = 4
+
+	print(n1 + n2)
+
+elif True:
+	print("Agora é verdadeiro.")
+	nome = input("Qual o seu nome? ")
+
+	print(f"Seu nome é {nome}")
+
+elif False:
+	print("Mais uma verdadeira.")
+	print(22 + 22)
+
+else:
+	print("Não é verdadeiro.")
+	print("Olá")
