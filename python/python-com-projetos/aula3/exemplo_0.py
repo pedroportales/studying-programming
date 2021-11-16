f3d6cc1b652ceaa4caa02fed539bbbb7ea37019f
Pedro Portales <pedro.portales@protonmail.com>
2021-11-15 23:05:08 -0300

    estrutura de dados

---
+++ python/python-com-projetos/aula3/exemplo_0.py
@@ -0,0 +1,15 @@
+"""
+Entrada de dados - Aula 3
+"""
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+
+ano_nasc = 2019 - idade
+
+print(f'\n{nome} tem {idade} anos. Ele nasceu em {ano_nasc}.')
+print()
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+print("\n", n1 + n2)
