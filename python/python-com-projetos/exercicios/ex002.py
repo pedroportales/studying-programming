275da4edad2a935679011c76934fa7b0c39224a2
Pedro Portales <pedroportales@yandex.com>
2021-12-14 21:45:51 -0300

    ex002 correction 2

--- python/python-com-projetos/exercicios/ex002.py
+++ python/python-com-projetos/exercicios/ex002.py
@@ -6,9 +6,12 @@ Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 
 time = int(input("Digite um horário (0-23): "))
 
-if time >= 0 and time <= 11:
-    print("Bom dia!")
-elif time >= 12 and time <= 17:
-    print("Boa tarde!")
-elif time >= 18 and time <= 23:
-    print("Boa noite!")
+try:
+    if time >= 0 and time <= 11:
+        print("Bom dia!")
+    elif time >= 12 and time <= 17:
+        print("Boa tarde!")
+    elif time >= 18 and time <= 23:
+        print("Boa noite!")
+except:
+    print("Digite um horário no formato pedido!")
