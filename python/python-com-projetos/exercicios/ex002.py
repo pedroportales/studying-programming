8f782c4630958ff74b2b19641315344ebe4e1ac7
Pedro Portales <pedroportales@yandex.com>
2021-12-14 21:47:07 -0300

    ex002 correction 3

--- python/python-com-projetos/exercicios/ex002.py
+++ python/python-com-projetos/exercicios/ex002.py
@@ -4,9 +4,11 @@ descrito, exiba a saudação apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
-time = int(input("Digite um horário (0-23): "))
+time = input("Digite um horário (0-23): ")
 
 try:
+    time = int(time)
+
     if time >= 0 and time <= 11:
         print("Bom dia!")
     elif time >= 12 and time <= 17:
