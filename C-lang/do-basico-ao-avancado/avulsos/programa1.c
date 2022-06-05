05ca9b870b4228f5f30e7eed39985a14f21041f4
Pedro Portales <pedroportales@riseup.net>
2022-06-05 15:06:55 -0300

    variáveis C

---
+++ C-lang/do-basico-ao-avancado/avulsos/programa1.c
@@ -0,0 +1,14 @@
+// Aula sobre variáveis
+#include <stdio.h>
+
+int main() {
+  // Declarando variáveis
+  int idade; // Inteiro
+  
+  // Função para imprimir algo no console
+  printf("Qual é a sua idade? ");
+  scanf("%d", &idade); // Recebendo dados do usuário
+  printf("A sua idade é %d\n", idade); // Imprimindo dado recebido
+  
+  return 0;
+}
