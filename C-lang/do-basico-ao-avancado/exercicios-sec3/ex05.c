2593d782c57e0b8e76cbc52d98fddc0ba09c35a7
Pedro Portales <pedroportales@riseup.net>
2022-06-09 21:59:57 -0300

    ex05 c

---
+++ C-lang/do-basico-ao-avancado/exercicios-sec3/ex05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  float n1;
+
+  printf("Digite um número real: ");
+  scanf("%f", &n1);
+
+  printf("A quinta parte desse número é %.2f\n", n1/5);
+  
+  return 0;
+}
+
