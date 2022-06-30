35781cd77c47671e526f4f57ff133b3646a940b3
Pedro Portales <pedroportales@riseup.net>
2022-06-29 23:14:32 -0300

    aula04 lua

---
+++ lua/introducao/aula04.lua
@@ -0,0 +1,14 @@
+local resultado = 4 - 10
+print( resultado )
+
+local resultado = -34
+print( resultado )
+
+local numeroReal = 45.5
+print( numeroReal )
+
+local outroNumero = -3.1
+print( outroNumero )
+
+local pi = 3.14159265358979323
+print( "PI " .. pi )
