6094c85fef459fb8fa9eee063a712cee4ec6fb5c
Pedro Portales <pedroportales@riseup.net>
2022-06-29 22:43:03 -0300

    introduction lua

---
+++ lua/introducao/aula02-03.lua
@@ -0,0 +1,9 @@
+-- boolean só pode assumir dois possíveis valores
+-- True or False
+
+local estaAtivo = false
+print( estaAtivo )
+print( estaAtivo == false )
+
+local estaAtivo = true
+print( "Valor de estaInativo " .. tostring( estaAtivo ) )
