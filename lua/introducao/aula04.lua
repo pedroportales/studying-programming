6d947a325d460782cd2f0105446a752fd9568bf0
Pedro Portales <pedroportales@riseup.net>
2022-06-30 22:29:01 -0300

    aula04 lua complete

--- lua/introducao/aula04.lua
+++ lua/introducao/aula04.lua
@@ -12,3 +12,15 @@ print( outroNumero )
 
 local pi = 3.14159265358979323
 print( "PI " .. pi )
+
+local umaVariavel = nil
+print( umaVariavel )
+
+local escape = "teste de \"escape\" de texto"
+print( escape )
+
+local quebraLinha = "linha 1\nlinha 2"
+print( quebraLinha )
+
+local tab = "\tLinha tabulada\n\rLinha não tabulada"
+print( tab )
