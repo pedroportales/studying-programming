9ac28c093c71dbdb87b2aad522195ae8814e4380
Pedro Portales <pedroportales@riseup.net>
2022-11-23 20:25:28 +0000

    update aula05.lua

--- lua/introducao/aula05.lua
+++ lua/introducao/aula05.lua
@@ -1,5 +1,9 @@
+-- io = conjunto de funções relacionadas a entrada e saída de dados
+
 io.write("Qual é o seu nome?")
 local nome = io.read() -- io = conjunto de funções relacionadas a entrada e saída de dados
+
 io.write("Então o seu nome é " .. nome .. ". Qual é o seu sobrenome?")
 local sobrenome = io.read()
+
 io.write("Ah, seu nome completo é: " .. nome .. " " .. sobrenome)
