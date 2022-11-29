99133606e2a378a7dcbe01b7a25d9193769ea63a
Pedro Portales <pedroportales@riseup.net>
2022-11-29 17:13:41 +0000

    if

---
+++ lua/introducao/aula09.lua
@@ -0,0 +1,13 @@
+-- Condição Se - Então e o s operadores if, else
+
+io.write("Digite um valor: ")
+local valor1 = io.read()
+io.write("Digite outro valor: ")
+local valor2 = io.read()
+
+if valor1 > valor2 then
+    print("O primeiro valor é maior que o segundo valor")
+
+else
+    print("O segundo valor é maior que o primeiro valor")
+end
