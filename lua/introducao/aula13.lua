a68fdab924d37bd878d40f2e01f7635957e5d2f4
Pedro Portales <pedroportales@riseup.net>
2022-11-30 18:02:47 +0000

    not and or

---
+++ lua/introducao/aula13.lua
@@ -0,0 +1,11 @@
+if not((not false) or (not true)) then
+    print("Lâmpada acesa!")
+end
+
+if false or true then
+    print("Lâmpada acesa!")
+end
+
+if not(false) or true then
+    print("Lâmpada acesa!")
+end
