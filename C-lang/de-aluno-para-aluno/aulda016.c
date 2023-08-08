bb8b21a08981a0bde2b26fc8ecee642e3f4896c9
Pedro Portales <pedroportales@riseup.net>
2023-08-07 21:24:58 -0300

    aula016 C

---
+++ C-lang/de-aluno-para-aluno/aulda016.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+
+    printf("Insira um número inteiro de 1 a 5");
+    scanf("%i", &i);
+
+    switch (i) {
+        case 1:
+            printf("primeiro\n");
+            break;
+        case 2:
+            printf("segundo\n");
+            break;
+        case 3:
+            printf("terceiro\n");
+            break;
+        case 4:
+            printf("quarto\n");
+            break;
+        case 5:
+            printf("quinto\n");
+            break;
+        default:
+            printf("opção não válida\n");
+            break;
+    }
+
+
+    return 0;
+}
