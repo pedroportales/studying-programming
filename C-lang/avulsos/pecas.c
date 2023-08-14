03015c0943631323686a3e2b8f4932c03a69056e
Pedro Portales <pedroportales@riseup.net>
2023-08-14 19:43:50 +0000

    update pecas.c

--- C-lang/avulsos/pecas.c
+++ C-lang/avulsos/pecas.c
@@ -2,8 +2,12 @@
 
 int main(){
     // Entrada
+    char vendedor[10];
     int codigo, quantidade;
-    float valor, valortotal;
+    float comissao, valor, valortotal;
+
+    printf("Insira o nome do vendedor: ");
+    scanf("%s", vendedor);
 
     printf("Insira o código do produto: ");
     scanf("%i", &codigo);
@@ -16,11 +20,12 @@ int main(){
 
     // Processamento
     valortotal = valor * quantidade;
+    comissao = 0.05 * valortotal;
     
     // Saída
-    printf("O pedido foi realizado! Código do produto pedido: %i\n", codigo);
+    printf("O pedido foi realizado pelo vendedor %s com sucesso! Código do produto pedido: %i\n", vendedor, codigo);
     printf("O valor individual do produto é %0.2f, e a quantidade pedida foi %i\n", valor, quantidade);
     printf("O valor total do pedido é R$%0.2f\n", valortotal);
-    
+
     return 0;
 }
