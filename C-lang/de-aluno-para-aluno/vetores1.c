ef4691286a8c37c954b0f875020030f07d2b14ec
Pedro Portales <pedroportales@riseup.net>
2023-09-19 23:21:57 +0000

    array c

---
+++ C-lang/de-aluno-para-aluno/vetores1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+
+    int nota[5];
+
+    nota[0] = 1;
+    nota[1] = 2;
+    nota[2] = 3;
+    nota[3] = 4;
+    nota[4] = 5;
+    nota[5] = 6;
+
+    printf("%d\n", nota[5]);
+
+    return 0;
+}
