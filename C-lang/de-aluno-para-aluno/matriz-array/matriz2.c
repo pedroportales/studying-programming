df87f558b3f6c696d86ba6b120494257c368c2fe
Pedro Portales <pedroportales@riseup.net>
2023-09-20 17:42:26 +0000

    completed matriz2.c

--- C-lang/de-aluno-para-aluno/matriz-array/matriz2.c
+++ C-lang/de-aluno-para-aluno/matriz-array/matriz2.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
-
-void estudantes(){
-    printf("Estudantes: \n");
-    printf("1. Estudante 1 \n");
-    printf("2. Estudante 2 \n");
-    printf("3. Estudante 3 \n");
-    printf("4. Estudante 4 \n");
-
-}
+#include <stdlib.h>
 
 int main(void){
-    estudantes();
 
     int m1[3][2], m2[3][2], soma[3][2];
 
-    printf("Valores primeira matriz: ");
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 2; j++){
-            scanf("%d ", &m1[i][j]);
+    // Primeira matriz
+    for(int i = 0; i < 3; ++i){
+        for(int j = 0; j < 2; ++j){
+            printf("A[%d][%d]: ",i,j);
+            scanf("%d", &m1[i][j]);
         }
     }
 
-    printf("Valores segunda matriz: ");
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 2; j++){
-            scanf("%d ", &m2[i][j]);
+    printf("\n");
+
+    // Segunda matriz
+    for(int i = 0; i < 3; ++i){
+        for(int j = 0; j < 2; ++j){
+            printf("B[%d][%d]: ", i, j);
+            scanf("%d", &m2[i][j]);
         }
     }
 
-    printf("Soma das matrizes: \n");
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 2; j++){
-            printf("%d ", soma[i][j]);
+    for(int i = 0; i < 3; ++i){
+        for(int j = 0; j < 2; ++j){
+            soma[i][j] = m1[i][j] + m2[i][j];
+            printf("%d\t", soma[i][j]);
         }
         printf("\n");
     }
-
-
 
     return 0;
 }
