fcc8b2ee9b799279881b917267a121b8e571de6f
Pedro Portales <pedroportales@riseup.net>
2023-10-19 20:13:20 -0300

    functions in C

---
+++ C-lang/de-aluno-para-aluno/funcoes/aula028.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+    void imprimaMensagem(void);
+
+    imprimaMensagem();
+
+    return 0;
+}
+
+void imprimaMensagem(void){
+    printf("Test");
+}
---
+++ C-lang/de-aluno-para-aluno/funcoes/aula029.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+
+    float calcularAreaRetang(float x, float y);
+    
+    float area = calcularAreaRetang(10.0, 20.0);
+
+    printf("A área é: %.2f\n", area);
+
+
+    return 0;
+}
+
+float calcularAreaRetang(float base, float altura){
+
+    float area = base * altura;
+
+    return area;
+}
